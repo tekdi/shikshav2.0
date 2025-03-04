557773e6366a931fa2eb66d58d96db3a926b16de
sagar <sagarkoshti1990@gmila.com>
2025-03-04 18:21:43 +0530

    fix: content plyear detail page

--- apps/atree-app/src/pages/player/[identifier].tsx
+++ apps/atree-app/src/pages/player/[identifier].tsx
@@ -1,18 +1,39 @@
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { SunbirdPlayer } from '@shared-lib';
+import Layout from '../../component/layout/layout';
+import { Typography } from '@mui/material';
 interface PlayerPageProps {
   id: string; // Define the type for the 'id' prop
 }
 const PlayerPage: React.FC<PlayerPageProps> = ({ id }) => {
   const router = useRouter();
   const { identifier } = router.query; // Access the identifier from the URL
-  console.log('id', identifier);
   if (!identifier) {
     return <div>Loading...</div>;
   }
 
-  return <SunbirdPlayer identifier={id ? id : (identifier as string)} />;
+  return (
+    <Layout
+      isFooter={false}
+      backIconClick={() => router.back()}
+      showBack
+      _backButton={{ alignItems: 'center' }}
+      backTitle={
+        <Typography
+          style={{
+            fontSize: '22px',
+            lineHeight: '28px',
+            letterSpacing: 0,
+          }}
+        >
+          {'Content Player'}
+        </Typography>
+      }
+    >
+      <SunbirdPlayer identifier={id ? id : (identifier as string)} />
+    </Layout>
+  );
 };
 
 export default PlayerPage;
